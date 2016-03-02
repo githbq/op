5d19162538bcbb147d29bc52953019a4c967a4e2
hubq <hubq@firstshare.cn>
2016-03-02 18:07:53 +0800

    '11'

--- src/resources/common/widget/slider/slider.js
+++ src/resources/common/widget/slider/slider.js
@@ -20,8 +20,7 @@ define(function(require, exports, module){
 		
 		init: function(attrs){
 			var me = this;
-			alert('Slider===me.prototype.constructor=>'+Slider===me.prototype.constructor)
-			me.attrs = me.prototype.constructor.merge( {} , me.defaultAttr ,attrs );
+			me.attrs = Slider.merge( {} , me.defaultAttr ,attrs );
 
 			me.$view = $(me.view);
 			me.$view.find('.slider-body').html( me.content );
