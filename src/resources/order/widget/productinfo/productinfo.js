bca70ce339d1ba6f17bc03f65551395b5eaf5a9d
hubq <hubq@firstshare.cn>
2016-03-06 17:05:24 +0800

    '111'

--- src/resources/order/widget/productinfo/productinfo.js
+++ src/resources/order/widget/productinfo/productinfo.js
@@ -42,17 +42,18 @@ define(function (require, exports, module) {
             data.terminalInfo.$view.addClass('productinfo');
             formInfo = new FormInfo({view: data.formInfo.$view, dataItems: data.formInfo.dataItems || formDataItems});
         }
-        return {terminalInfo: terminalInfo, tableInfo: tableInfo, formInfo: formInfo};
+        return {terminalInfo: terminalInfo, tableInfo: tableInfo, formInfo: formInfo,data:transferDataByType(type)};
     };
 
     function getXinGouNormal(terminalInfo, tableInfo, formInfo) {
 
 
     }
+
     //转换数据
     function transferDataByType(type) {
-        type=type||'1';
-        var data={order:{},subOrders:[],contract:{}};
+        type = type || '1';
+        var data = {order: {}, subOrders: [], contract: {},enterpriseExtend:{}};
         switch (config[type]) {
             case '办公版新购-普通':
             {
@@ -60,42 +61,60 @@ define(function (require, exports, module) {
                 var tableInfoData = tableInfo.o_getValues();
                 var formInfoData = formInfo.o_getValues();
 
-
-
+                //终端部分//////////////////////////////////
+                //销客终端总量
                 data.subOrders.push({
                     purchaseCount: terminalInfoData.purchaseCount_0,
-                    subOrderType:0,
-                    purchaseAmount:null,
-                    startTime:terminalInfoData.startTime_0,
-                    endTime:terminalInfoData.endTime_0
+                    subOrderType: 0,
+                    purchaseAmount: null,
+                    startTime: terminalInfoData.startTime_0,
+                    endTime: terminalInfoData.endTime_0
                 });
-
+                //服务人数
                 data.subOrders.push({
                     purchaseCount: terminalInfoData.purchaseCount_1,
-                    subOrderType:1,
-                    purchaseAmount:terminalInfoData.purchaseAmount_1,
-                    startTime:null,
-                    endTime:null
+                    subOrderType: 1,
+                    purchaseAmount: terminalInfoData.purchaseAmount_1,
+                    startTime: null,
+                    endTime: null
                 });
 
-
-                data.subOrders.push({
-                    purchaseCount: terminalInfoData.purchaseCount_1,
-                    subOrderType:1,
-                    purchaseAmount:terminalInfoData.purchaseAmount_1,
-                    startTime:null,
-                    endTime:null
-                });
-
-
-
-
-
-
-
-
-
-
+                //表格部分 //////////////////////////////////////////
+                var ids = ['11', '22', '33', '44'];
+                var checkeds = tableInfo.check.split(',');
+                $(ids).each(function (i, n) {
+                        if ($.inArray(n, checkeds) >= 0) {
+                            data.subOrders.push({
+                                purchaseCount: tableInfoData['purchaseCount_' + n],
+                                subOrderType: 11,
+                                purchaseAmount: tableInfoData['purchaseAmount_' + n],
+                                startTime: tableInfoData['startDate_' + n],
+                                endTime: tableInfoData['endDate_' + n],
+                                productAmount: tableInfoData['productAmount_' + n]
+                            });
+                        }
+                    }
+                );
+                //合同部分//////////////////////////////////////////////////////////////////////////
+                var contractData= $.parseJSON(formInfoData.contract||'{}');
+                var contractCopyData= $.parseJSON(formInfoData.contractCopy||'{}');
+                data.contract={
+                    contractNo:formInfoData.contractNo,
+                    contractPrice:formInfoData.contractPrice,
+                    sealName:formInfoData.sealName,
+                    contract:contractData.contract,
+                    contractFileName:contractData.contractFileName,
+                    contractCopy:contractCopyData.contractCopy,
+                    contractCopyFileName:contractCopyData.contractCopyFileName
+                };
+                //门头照片
+                var companyGatePictureData= $.parseJSON(formInfoData.companyGatePicture||'{}');
+                data.enterpriseExtend={
+                    compayGatePicture:companyGatePictureData.compayGatePicture,
+                    compayGatePictureFileName:companyGatePictureData.compayGatePictureFileName,
+                    companyGateKeyword:formInfoData.companyGateKeyword,
+                    companyGateRemark:formInfoData.companyGateRemark
+                };
             }
                 break;
         }
