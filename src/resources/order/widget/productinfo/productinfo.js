a7460bbb4d42d5aaec2eacb755d93418cd127e15
hubq <hubq@firstshare.cn>
2016-03-07 13:27:51 +0800

    'parseJSON'

--- src/resources/order/widget/productinfo/productinfo.js
+++ src/resources/order/widget/productinfo/productinfo.js
@@ -123,8 +123,8 @@ define(function (require, exports, module) {
                 );
                 debugger
                 //合同部分//////////////////////////////////////////////////////////////////////////
-                var contractData = $.parseJSON('{}' || formInfoData.contract);
-                var contractCopyData = $.parseJSON('{}' || formInfoData.contractCopy);
+                var contractData = $.parseJSON( formInfoData.contract||'{}');
+                var contractCopyData = $.parseJSON( formInfoData.contractCopy||'{}');
                 data.contract = {
                     contractNo: formInfoData.contractNo,
                     contractPrice: formInfoData.contractPrice,
@@ -135,7 +135,7 @@ define(function (require, exports, module) {
                     contractCopyFileName: contractCopyData.contractCopyFileName
                 };
                 //门头照片
-                var companyGatePictureData = $.parseJSON('{}' || formInfoData.companyGatePicture);
+                var companyGatePictureData = $.parseJSON( formInfoData.companyGatePicture||'{}');
                 data.enterpriseExtend = {
                     compayGatePicture: companyGatePictureData.compayGatePicture,
                     compayGatePictureFileName: companyGatePictureData.compayGatePictureFileName,
