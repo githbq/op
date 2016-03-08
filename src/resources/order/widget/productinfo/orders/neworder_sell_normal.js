e64a27ef2c28ea2dea6cfc873885a1f2442c619a
hubq <hubq@firstshare.cn>
2016-03-08 21:26:01 +0800

    '11'

--- src/resources/order/widget/productinfo/orders/neworder_sell_normal.js
+++ src/resources/order/widget/productinfo/orders/neworder_sell_normal.js
@@ -7,8 +7,6 @@ define(function (require, exports, module) {
             var tableInfoData = tableInfo.o_getValues();
             var formInfoData = formInfo.o_getValues();
 
-            //终端部分//////////////////////////////////
-
             //销客终端总量
             data.subOrders.push({
                     subOrder: {
@@ -51,7 +49,8 @@ define(function (require, exports, module) {
                                 startTime: tableInfoData['startDate_' + n],
                                 endTime: tableInfoData['endDate_' + n],
                                 productAmount: tableInfoData['productAmount_' + n],
-                                discount: tableInfoData['discount_' + n]
+                                discount: tableInfoData['discount_' + n],
+                                currPayAmount:formInfoData['currPayAmount_'+n]||0
                             }
                         });
                     }
@@ -93,10 +92,51 @@ define(function (require, exports, module) {
             return data;
         }
     };
-    //转换输入值
-    exports.transferDataItem = function (terminalDataItems, tableDataItems, formDataItems) {//转换数据项
 
-        return {terminalDataItems:terminalDataItems,tableDataItems:tableDataItems,formDataItems:formDataItems};
+
+    //转换输入值
+    exports.transferDataItem = function (terminalDataItems, tableDataItems, formDataItems, controller) {//转换数据项
+        debugger
+        controller(terminalDataItems, 'type_8', function (n) {
+            n.visible = false;
+        });
+        controller(terminalDataItems, 'purchaseAmount_input_8', function (n) {
+            n.visible = false;
+        });
+        controller(terminalDataItems, 'purchaseAmount_wrapper_3', function (n) {
+            n.visible = true;
+        });
+        controller(terminalDataItems, 'purchaseAmount_input_3', function (n) {
+            n.visible = true;
+        });
+        controller(terminalDataItems, 'useCRMWrapper', function (n) {
+            n.visible = true;
+        });
+        controller(terminalDataItems, 'purchaseAmount_3', function (n) {
+            n.visible = false;
+        });
+
+        controller(terminalDataItems, 'businesscard', function (n) {
+            n.visible = true;
+        });
+        controller(formDataItems, 'payStatus_name', function (n) {
+            n.visible = false;
+        });
+        controller(formDataItems, 'payStatus_select', function (n) {
+            n.visible = true;
+        });
+        debugger
+        controller(tableDataItems, 'table_type', function (n) {
+            n.visible = false;
+        });
+        //$(['currPayAmount_1', 'currPayAmount_2', 'currPayAmount_3', 'currPayAmount', 'currPayAmount_5', 'currPayAmount_7']).each(function (i, n) {
+        //    controller(formDataItems, n, function (n) {
+        //        n.visible = false;
+        //    });
+        //});
+
+
+        return {terminalDataItems: terminalDataItems, tableDataItems: tableDataItems, formDataItems: formDataItems};
     }
 
 });
