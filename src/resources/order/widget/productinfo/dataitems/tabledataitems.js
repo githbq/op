f9f24e70a09467cad5f2acebd63bbeba6ea8eb76
hubq <hubq@firstshare.cn>
2016-03-10 18:48:51 +0800

    '11'

--- src/resources/order/widget/productinfo/dataitems/tabledataitems.js
+++ src/resources/order/widget/productinfo/dataitems/tabledataitems.js
@@ -242,8 +242,8 @@ define(function (require, exports, module) {
             productAmount += parseFloat(purchaseModule.o_getFieldValue('productAmount_' + id) || 0);
 
         });
-        me.__refs.terminalInfo.o_setValue({name: 'startTime_2', value: smallStartDate ? smallStartDate : null});
-        me.__refs.terminalInfo.o_setValue({name: 'endTime_2', value: maxEndDate ? maxEndDate : null});
+        //me.__refs.terminalInfo.o_setValue({name: 'startTime_2', value: smallStartDate ? smallStartDate : null});
+        //me.__refs.terminalInfo.o_setValue({name: 'endTime_2', value: maxEndDate ? maxEndDate : null});
         console.log('合同总金额之表格部分计算结果1:' + me.o_getFieldValue('order_amount'));
 
 
