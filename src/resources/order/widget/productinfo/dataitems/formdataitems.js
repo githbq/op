9cd38685960cfdc8e9456f62856c8ae024591cbe
hubq <hubq@firstshare.cn>
2016-03-18 13:22:59 +0800

    'required'

--- src/resources/order/widget/productinfo/dataitems/formdataitems.js
+++ src/resources/order/widget/productinfo/dataitems/formdataitems.js
@@ -279,13 +279,13 @@ define(function (require, exports, module) {
                 }
             ],
             validateOptions:{
-                required:{
-                    enable:true,value:true,handler:function(error, value, option, $ele){
-                        if(!this.o_getFieldValue('contract')){
-                            return error = {field: $ele, name: 'required', option: option};
-                        }
-                    }
-                }
+                //required:{
+                //    enable:true,value:true,handler:function(error, value, option, $ele){
+                //        if(!this.o_getFieldValue('contract')){
+                //            return error = {field: $ele, name: 'required', option: option};
+                //        }
+                //    }
+                //}
             }
         }));
         //合同副本照片
