4687bc407182778dd70c5417a78673185625da9c
hubq <hubq@firstshare.cn>
2016-03-18 13:28:35 +0800

    ';11'

--- src/resources/order/widget/productinfo/dataitems/formdataitems.js
+++ src/resources/order/widget/productinfo/dataitems/formdataitems.js
@@ -279,13 +279,14 @@ define(function (require, exports, module) {
                 }
             ],
             validateOptions:{
-                //required:{
-                //    enable:true,value:true,handler:function(error, value, option, $ele){
-                //        if(!this.o_getFieldValue('contract')){
-                //            return error = {field: $ele, name: 'required', option: option};
-                //        }
-                //    }
-                //}
+                required:{
+                    enable:true,value:true,handler:function(error, value, option, $ele){
+                        if(!this.o_getFieldValue('contract')){
+                            return error = {field: $ele, name: 'required', option: option};
+                        }
+                        return false;
+                    }
+                }
             }
         }));
         //合同副本照片
