9b7369d37146b3da2ba4690a5562e0cd1bd02854
hubq <hubq@firstshare.cn>
2016-03-25 19:11:54 +0800

    'enabled'

--- src/resources/order/widget/productinfo/orders/index.js
+++ src/resources/order/widget/productinfo/orders/index.js
@@ -102,7 +102,9 @@ define(function (require, exports, module) {
                 var useFX = false;
                 $(subOrders).each(function (i, n) {
                     if (n.subOrder && n.subOrder.productId && n.subOrder.productId != 10 && n.subOrder.productId != 11) {//10为绑定百川  11为绑定报数系统
-                        checkids.push(n.subOrder.productId);
+                        if (n.subOrder.enabled !== false) {
+                            checkids.push(n.subOrder.productId);
+                        }
                         var subOrder = n.subOrder;
                         for (var j in subOrder) {
                             if (subOrder.hasOwnProperty(j)) {
