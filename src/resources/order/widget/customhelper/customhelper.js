ebc08819490792fac85fe75bb738aae0aab44926
liyl <liyl@fxiaoke.com>
2016-03-31 15:13:43 +0800

    * lianghsit

--- src/resources/order/widget/customhelper/customhelper.js
+++ src/resources/order/widget/customhelper/customhelper.js
@@ -72,13 +72,13 @@ define( function(require, exports, module){
 
 			me.$customHelper.on('input',function(){
 				var customer = me.$customHelper.val();
+				if(!customer){
+					return false;
+				}
 				util.api({
-					'url': '~/op/api/s/activity/getcompanys',
+					'url': '~/op/api/order/enterprise/getAccountForEnterprisePartner',
 					'data': {
-						'companyName': customer,
-						'companyType': 0,
-						'pageIndex': 0,
-						'pageSize': 10
+						'accountname': customer
 					},
 					'success': function (data) {
 						console.warn(data);
@@ -87,8 +87,8 @@ define( function(require, exports, module){
 							var strDom =''; 
 							me.$('.list').show();
 							me.$('.list').empty();
-							me.attrs.listObjet = data.value.model.content;
-							data.value.model.content.forEach(function (item) {
+							me.attrs.listObjet = data.value.model;
+							data.value.model.forEach(function (item) {
 								strDom += '<li class="list-item" data-id="'+item.id+'"><a>'+item.name+'</a></li>'
 								
 							});
@@ -105,11 +105,12 @@ define( function(require, exports, module){
 				me.delCustomHelper(id);
 			})
         },
-        show: function(opitons){
+        show: function(options){
             
             CustomHelper.__super__.show.apply(this,arguments);
 			
 			var me = this;
+			me.attrs.options = options||{};
 			me.attrs.list= [];
 			me.getCustomHelper();
           
@@ -127,12 +128,11 @@ define( function(require, exports, module){
 					console.warn(data);
 					if (data.success) {
 						
-					
+						me.attrs.list = data.value.model;
+						me.showCustomHelper();
 					}
 				}
 			})
-			
-			me.showCustomHelper();
 			
 		},
 		//展示现有跟进人：
@@ -141,7 +141,7 @@ define( function(require, exports, module){
 			 me.$view.find('.custom-box').empty();
 			var obj = me.attrs.list;
 			for(var i=0;i<obj.length;i++){
-				me.$view.find('.custom-box').append(' <a class="badge" data-id="'+obj[i].id+'" >'+obj[i].name+' x </a>');
+				me.$view.find('.custom-box').append(' <a class="badge" data-id="'+obj[i].accountId+'" >'+obj[i].accountName+' x </a>');
 			}
 		},
 		//删除跟进人：
@@ -149,7 +149,7 @@ define( function(require, exports, module){
 			var me = this;
 			
 			for(var i=0;i< me.attrs.list.length;i++){
-				if( me.attrs.list[i].id == id){
+				if( me.attrs.list[i].accountId == id){
 					me.attrs.list.splice(i,1)
 					return false;
 				}
@@ -160,16 +160,17 @@ define( function(require, exports, module){
 			var me = this;
 			if(me.$customHelper.val() && me.attrs.list.length<5){
 				var personObj={};
-				var id = me.$customHelper.attr('data-id');
+				var accountId = me.$customHelper.attr('data-id');
 				for(var i=0;i<me.attrs.list.length;i++){
-					if(me.attrs.list[i].id==id){
+					if(me.attrs.list[i].accountId==accountId){
 						util.showToast('跟进人已存在');
 						return false;
 					}
 				}
-				personObj.id=id;
-				personObj.name=me.$customHelper.val();
-				me.$view.find('.custom-box').append(' <a class="badge" data-id="'+id+'" >'+me.$customHelper.val()+' x </a>');
+	
+				personObj = {'id':'','epId':me.attrs.options.enterpriseId,'accountId':accountId,'accountName':me.$customHelper.val()}
+
+				me.$view.find('.custom-box').append(' <a class="badge" data-id="'+accountId+'" >'+me.$customHelper.val()+' x </a>');
 				me.attrs.list.push(personObj);
 			}
 		},
@@ -180,6 +181,26 @@ define( function(require, exports, module){
 		//保存联合跟进人
 		actionSaveEve:function(){
 			var me = this;
+			
+			if(me.attrs.list.length<1){
+				util.showToast('请添加联合跟进人！');
+				return false;
+			}
+
+			util.api({
+				'url': '~/op/api/order/enterprise/saveEnterprisePartners',
+				'data': {
+					'enterpriseId': me.attrs.options.enterpriseId,
+					'json':JSON.stringify( me.attrs.list )
+				},
+				'success': function (data) {
+					console.warn(data);
+					if (data.success) {
+						util.showTip('保存成功')
+						me.hide();
+					}
+				}
+			});
 			
 		},
 
@@ -192,6 +213,5 @@ define( function(require, exports, module){
        
 	})
 
-        
 	module.exports = CustomHelper;
 });
