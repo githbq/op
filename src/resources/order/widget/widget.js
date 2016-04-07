379e4e05049f6eb2148804adbbdbaa53a08366f0
hubq <hubq@firstshare.cn>
2016-04-07 10:12:26 +0800

    '11'

--- src/resources/order/widget/widget.js
+++ src/resources/order/widget/widget.js
@@ -27,8 +27,6 @@ define(function (require, exports, module) {
 
         });
         $('.test2').click(function () {
-            alert('结果')
-            debugger
             var aaa = result.getData();
             console.log('显示结果');
             console.warn(aaa);
@@ -50,6 +48,62 @@ define(function (require, exports, module) {
 
 
 
-
-
-
+//
+//
+//
+//
+//define(function (require, exports, module) {
+//
+//    var productInfo = require('./productinfo/productinfo');
+//
+//    exports.init = function () {
+//        var me = this;
+//        var $el = exports.$el;
+//        var result = null;
+//        $('.test').click(function () {
+//            var type = $('#inputtype').val();
+//            if (type && parseInt(type))
+//                result = productInfo.showProductInfo({
+//                    terminalInfo: {$view: $el.find('.panel1')},
+//                    tableInfo: {$view: $el.find('.panel2')},
+//                    formInfo: {$view: $el.find('.panel3')}
+//                }, parseInt(type));
+//        });
+//        $('#changeType').change(function () {
+//            if (!$(this).val()) {
+//                return;
+//            }
+//            result = productInfo.showProductInfo({
+//                terminalInfo: {$view: $el.find('.panel1')},
+//                tableInfo: {$view: $el.find('.panel2')},
+//                formInfo: {$view: $el.find('.panel3')}
+//            }, parseInt($(this).val()));
+//
+//        });
+//        $('.test2').click(function () {
+//            alert('结果')
+//            debugger
+//            var aaa = result.getData();
+//            console.log('显示结果');
+//            console.warn(aaa);
+//            debugger
+//        });
+//        $('#testValidate').click(function () {
+//
+//            debugger
+//            var aaa = result.validate();
+//            debugger
+//        });
+//
+//        //result.data
+//    }
+//});
+//
+//
+//
+//
+//
+//
+//
+//
+//
