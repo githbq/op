74d1efd06ceed0531d3ebe8d0590515d3e6c554f
hubq <hubq@firstshare.cn>
2016-04-12 22:30:02 +0800

    '11'

---
+++ src/resources/index/rpt2/rpt2.js
@@ -0,0 +1,108 @@
+define(function (require, exports, module) {
+    var IBSS = window.IBSS;
+
+    var tpl = $(require('./template.html'));
+
+    var EnterpriseReportTable = MClass(M.Center).include({
+        tplReport: _.template(tpl.filter('#trReport').html()),
+        elements: {
+            'tbody': 'tbody',
+            '.chooseDate': 'chooseDate',
+            '.selectType': 'selectType',
+            '.download': 'download'
+        },
+        events: {
+            'click .download': 'generate'
+        },
+        init: function () {
+            EnterpriseReportTable.__super__.init.apply(this, arguments);
+            var me = this;
+            me.$chooseDate.datetimepicker({'timepicker': false, 'format': 'Y/m/d'});
+        },
+        generate: function (e) {
+            debugger
+            var me = this;
+            if(!me.$chooseDate.val()){
+                util.showToast('请填日期');
+               return;
+            }
+
+            var data = {type: me.$selectType.val(), date: new Date(me.$chooseDate.val() + " 00:00:00").getTime()};
+            window.open('/op/api/file/downloadfromhdfs?type='+data.type+'&date='+data.date,'_blank');//'hideiframe');
+            //util.api({
+            //    type:'get',
+            //    url: '~/op/api/file/downloadfromhdfs',
+            //    data: data,
+            //    success: function (data) {
+            //        if (data.success) {
+            //            me.load();
+            //        }
+            //    }
+            //});
+        },
+        load: function () {
+            var me = this;
+            util.api({
+                url: '/query/act/getmonth',
+                data: {},
+                beforeSend: function () {
+                    me.$tbody.html('<tr><td colspan="8"><p class="info">加载中...</p></td></tr>');
+                },
+                success: function (data) {
+                    if (data.success) {
+                        me.collection.init(data.model);
+                        var content = me.collection.all();
+                        if (content.length > 0) {
+                            me.$tbody.html(me.tplReport({'content': me.collection.all()}));
+                            IBSS.tplEvent.setPermissions(me.$tbody);
+                        } else {
+                            me.$tbody.html('<tr><td colspan="8"><p class="info">暂无数据</p></td></tr>');
+                        }
+                    } else {
+                        me.$tbody.html('<tr><td colspan="8"><p class="info">数据加载失败</p></td></tr>');
+                    }
+                },
+                error: function () {
+                    me.$tbody.html('<tr><td colspan="8"><p class="info">数据加载失败</p></td></tr>');
+                }
+            });
+        },
+
+        generateCollection: function (collection) {
+            var result = [], me = this;
+            var time = Date.parse(me.firstMonth + '/01 00:00:01');
+            for (var i = this.getMaxMonth(); i > time;) {
+                var d = new Date(i);
+                result.push({month: d._format('yyyyMM')});
+                d.setMonth(d.getMonth() - 1);
+                i = d.getTime();
+            }
+            $(collection).each(function (i, item) {
+                $.extend(me.getListItem(result, item.month), item);
+            });
+            return result;
+        },
+
+        getListItem: function (list, key) {
+            var result;
+            $(list).each(function (i, item) {
+                if (item.month == key) {
+                    result = item;
+                    return result;
+                }
+            });
+            return result;
+        },
+
+        getMaxMonth: function () {
+            var d = new Date();
+            d.setMonth(d.getMonth() - 1);
+            return d.getTime();
+        }
+    });
+
+    exports.init = function () {
+        var $el = exports.$el;
+        new EnterpriseReportTable({'view': $el.find('.m-act-rpt')});
+    }
+});
