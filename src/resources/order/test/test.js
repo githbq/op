ae4fc929671f458142e4674d92bef2c7ca158cbe
ligd <ligd@fxiaoke.com>
2016-04-14 13:27:36 +0800

    [测试]

--- src/resources/order/test/test.js
+++ src/resources/order/test/test.js
@@ -3,6 +3,7 @@ define(function( require , exports , module ){
 	var Invoice = require('../widget/invoice/invoice');
 	var InvoiceDetail = require('../widget/invoicedetail/invoicedetail');
 	var InvoiceInfo = require('../widget/invoicedetail/invoiceinfo');
+	var Express = require('../widget/express/express');
 
 
 
@@ -12,10 +13,15 @@ define(function( require , exports , module ){
 
 		var invoicedetail = new InvoiceDetail();
 		var invoiceinfo = new InvoiceInfo();
+		var express = new Express();
 		
 		$el.find('#getinfo').on('click',function(){
 			invoicedetail.show();
-		})
+		});
+
+		$el.find('#express').on('click',function(){
+			express.show();
+		});
 		/*
 		var invoice = new Invoice( {'wrapper':$el.find('.invoicewrapper')} );
 
