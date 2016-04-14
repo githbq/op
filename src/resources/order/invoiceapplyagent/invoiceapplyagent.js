1f739862eb2f1cccd7162bfe603b7381865c9b79
ligd <ligd@fxiaoke.com>
2016-04-14 18:59:10 +0800

    [repair]

--- src/resources/order/invoiceapplyagent/invoiceapplyagent.js
+++ src/resources/order/invoiceapplyagent/invoiceapplyagent.js
@@ -5,7 +5,24 @@ define(function (require, exports, module) {
     var Pagination = require('common/widget/pagination/pagination');
     var InvoiceDetail = require('../widget/invoicedetail/invoicedetail');
 
+    //付费状态map
+    var PayStatusMap = {
+        '1': '分期',
+        '2': '未付',
+        '3': '全款'
+    };
 
+    //发票类型
+    var InvoiceTypeMap = {
+        '1':'普通增值税发票',
+        '2':'增值税专用发票'
+    }
+
+    //发票性质
+    var InvoicePropMap = {
+        '1':'到款发票',
+        '2':'预开发票'
+    }
 
     //代理商发票申请列表
     var InvoiceApplyAgent = MClass( M.Center ).include({
@@ -28,7 +45,8 @@ define(function (require, exports, module) {
         },
         events:{
             'click .toggle b': 'switchEve',
-            'click .btn-search': 'searchEve'
+            'click .btn-search': 'searchEve',
+            'click .detail': 'detailEve'
         },
         switchEve: function( e ){
 
@@ -44,6 +62,17 @@ define(function (require, exports, module) {
             this.refresh();
         },
 
+        //查看发票详情
+        detailEve: function(e){
+            var me = this;
+
+            var inid = $(e.currentTarget).attr('data-inid');
+
+            var info = me.list.find('invoiceId',inid);
+            console.log( info );
+
+            me.trigger('detail' , info.orderId , inid , info.approvalStatus );
+        },
 
         //刷新审批列表
         refresh: function(){
@@ -83,14 +112,21 @@ define(function (require, exports, module) {
             util.api({
                 'url': url,
                 'data': data,
+                'beforeSend': function( data ){
+                    me.$('tbody').html("<tr><td colspan='12'><p class='info'>加载中......</p></td></tr>");
+                },
                 'success': function( data ){
                     if( data.success ){
                         console.warn( data );
                         me.pagination.setTotalSize( data.value.model.pageCount );
                         if( data.value.model.content.length > 0 ){
-                            me.list.reload( data.value.model.content );
+                            me.list.reload( data.value.model.content , function( item ){
+                                item.payStatusStr = PayStatusMap[item.payStatus];
+                                item.invoiceTypeStr = InvoiceTypeMap[item.invoiceType];
+                                item.invoicePropStr = InvoicePropMap[item.invoiceProp];
+                            });
                         } else {
-                            me.$('tbody').html("<tr><td colspan='12'><p class='info'>暂无数据</p></td></tr>")
+                            me.$('tbody').html("<tr><td colspan='12'><p class='info'>暂无数据</p></td></tr>");
                         }
                     }
                 }
@@ -105,6 +141,9 @@ define(function (require, exports, module) {
 
         //发票模块
         var invoiceDetail = new InvoiceDetail();
-        
+
+        invoiceApplyAgent.on('detail',function( orderId, inid , approvalStatus ){
+            invoiceDetail.show( orderId, inid, approvalStatus );
+        })
     }
 });
