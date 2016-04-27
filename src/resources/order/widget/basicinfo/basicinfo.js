cfe102d4f5b3ce118a8bca5d74b6adc5c0d03212
liyl <liyl@fxiaoke.com>
2016-04-27 14:56:00 +0800

    * 修改bug

--- src/resources/order/widget/basicinfo/basicinfo.js
+++ src/resources/order/widget/basicinfo/basicinfo.js
@@ -263,7 +263,7 @@ define(function(require, exports, module){
 			me.$('.required-basic').each(function(){
 				var $this = $( this );
 				var attr = $this.attr('ce-model');
-				if( me.model.get(attr)=='' || me.model.get(attr)==undefined  ){
+				if( me.model.get(attr)==='' || me.model.get(attr)=== undefined ||  me.model.get(attr) === null ){
 					util.warnInput( $this );
 					state = false;
 				}else{
