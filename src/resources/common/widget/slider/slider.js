f72d17fb3702c09336ce2d3c1325cfee18707b96
liyl <liyl@fxiaoke.com>
2016-04-27 17:18:39 +0800

    * 修改bug

--- src/resources/common/widget/slider/slider.js
+++ src/resources/common/widget/slider/slider.js
@@ -1,124 +1,155 @@
-define(function (require, exports, module) {
+define(function(require, exports, module){
+	
+	var sliderTpl = require('./slider.html');
+	
+	var $window = $(window);
 
-    var sliderTpl = require('./slider.html');
+	/*
+	*@ Slider基类 
+	*
+	*/
+	
+	/**
+	 *
+	 * $body 注册全局事件
+	 * 点击时 隐藏所有slider
+	 */
+	var $body = $('body');
+	$body.on('click',function(e){
+		if ($(e.target).closest('.m-slider').length == 0) {
+			for( var key in Slider.widgets ){
+				Slider.widgets[key].hide();
+			}
+		}
+	});
 
-    var $window = $(window);
 
-    /*
-     *@ Slider基类
-     *
-     */
-    var Slider = MClass(M.Center).include({
+	/**
+	 * @class Slider
+	 * 每生成一个对象 都会生成一个id
+	 * Slider类 将对象存放在Slider.widgets内以id分别
+	 */
+	var sid = 0;
 
-        view: sliderTpl,
-        content: '',            //内容html字符串
-        defaultAttr: {
-            'title': '',
-            'width': 500
-        },
+	var Slider = MClass(M.Center).include({
+		
+		view: sliderTpl,
+		content:'',            //内容html字符串
+		defaultAttr:{
+			'title':'',
+			'width':500
+		},
+		
+		init: function(attrs){
+			var me = this;
 
-        init: function (attrs) {
-            var me = this;
+			me.attrs = Slider.merge( {} , me.defaultAttr ,attrs );
 
-            me.attrs = Slider.merge({}, me.defaultAttr, attrs);
+			me.$view = $(me.view);
+			me.$view.find('.slider-body').html( me.content );
+			me.$view.find('.header-title').text(me.attrs['title']);
+			me.$view.css({'width':me.attrs['width']});
 
-            me.$view = $(me.view);
-            me.$view.find('.slider-body').html(me.content);
-            me.$view.find('.header-title').text(me.attrs['title']);
-            me.$view.css({'width': me.attrs['width']});
+			sid = sid + 1;
+			me.id = sid;
 
-            /**
-             *
-             * 向全局变量传递信息   页面switch时 会主动调用组件的destroy方法
-             * 同时对当前页面内元素进行权限校验
-             * 模板元素中涉及到权限的元素 如果可以显示 写行内样式让其显示
-             */
-            IBSS && IBSS.trigger && IBSS.trigger('_addWidget', me);
+			Slider.widgets[sid] = me;
 
-            Slider.__super__._initElements.apply(this, arguments);
-            Slider.__super__._initEvents.apply(this, arguments);
-            Slider.__super__._initDuplexing.apply(this, arguments);
+			/**
+			 *
+			 * 向全局变量传递信息   页面switch时 会主动调用组件的destroy方法
+			 * 同时对当前页面内元素进行权限校验
+			 * 模板元素中涉及到权限的元素 如果可以显示 写行内样式让其显示
+			 */
+			IBSS && IBSS.trigger && IBSS.trigger('_addWidget', me);
+			
+			Slider.__super__._initElements.apply( this , arguments );
+			Slider.__super__._initEvents.apply( this , arguments );
+			Slider.__super__._initDuplexing.apply( this , arguments );
 
-            me.isRender = false;
-            me.isShow = false;
+			me.isRender = false;
+			me.isShow = false;
 
-            me._initSliderEvents();
-        },
+			me._initSliderEvents();
+		},
 
-        _initSliderEvents: function () {
-            var me = this;
-            me.$view.on('click', '.header-close', function () {
-                me.hide()
-            });
+		_initSliderEvents: function(){
+			var me = this;
+			me.$view.on('click','.header-close',function(e){ e.stopPropagation(); me.hide()});
 
-            //body 事件注册
-            //body 点击时会隐藏slider
-            //$('body').on('click',function(e){
-            //	if ($(e.target).closest('.m-slider').length == 0) {
-            //		me.hide();
-            //	}
-            //});
-        },
+			//body 事件注册
+			//body 点击时会隐藏slider
+			//$('body').on('click',function(e){
+			//	if ($(e.target).closest('.m-slider').length == 0) {
+			//		me.hide();
+			//	}
+			//});
+			//me._onBodyClick = function(e){
+			//	if($(e.target).closest('.m-slider').length == 0){
+			//		me.hide();
+			//	}
+			//}
+			
+			//$body.on('click',me._onBodyClick);
+		},
 
-        //设置标题
-        _setTitle: function (title) {
-            this.$view.find('.header-title').text(title);
-        },
-        //设置标题
-        _setWidth: function (width) {
-            this.$view.css('width', width);
-        },
+		//设置标题
+		_setTitle: function( title ){
+			this.$view.find('.header-title').text( title );
+		},
 
-        render: function () {
-            var me = this;
+		render: function(){
+			var me = this;
 
-            $('body').append(me.$view);
-            me.isRender = true;
-        },
+			$('body').append(me.$view);
+			me.isRender = true;
+		},
 
-        //显示 滑动效果
-        show: function (noAnimate) {
-            var me = this;
-            if (!me.isRender) {
-                me.render();
-            }
+		//显示 滑动效果
+		show: function(){
+			var me = this;
+			if(!me.isRender){
+				me.render();
+			}
 
-            if (me.isShow === true) return;
+			if( me.isShow === true ) return;
 
-            me.$view.show();
-            var width = parseInt(me.attrs['width']);
-            me.isShow = true;
-            if (noAnimate) {
-                me.$view.css({'right': 0});
-            } else {
-                me.$view.css('right', -width).animate({'right': 0}, 200);
-            }
-        },
+			me.$view.show();
+			var width = parseInt( me.attrs['width'] );
+			me.isShow = true;
+			me.$view.css('right', -width).animate({'right':0},200);
+		},
 
-        //隐藏 滑动效果
-        hide: function () {
-            var me = this;
+		//隐藏 滑动效果
+		hide: function(){
+			var me = this;
 
-            if (me.isShow === false) return;
+			if( me.isShow === false ) return;
 
-            var width = parseInt(me.attrs['width']);
-            me.isShow = false;
-            me.$view.animate({'right': -width}, 200, function () {
-                me.$view.hide();
-            });
-        },
+			var width = parseInt( me.attrs['width'] );
+			me.isShow = false;
+			me.$view.animate({'right':-width},200,function(){
+				me.$view.hide();
+			});
+		},
 
-        // 移除
-        remove: function () {
-            Slider.__super__.remove.apply(this, arguments);
-        },
+		// 移除
+		remove: function(){
+			var me = this;
 
-        destroy: function () {
-            this.remove();
-        }
+			//$body.off('click',me._onBodyClick);
+			Slider.__super__.remove.apply(this,arguments);
+		},
 
-    });
+		destroy: function(){
+			this.remove();
+		}
 
-    module.exports = Slider;
+	});
+	
+	//初始化widgets数组
+	Slider.widgets = {};
+
+	module.exports = Slider;
 });
 
