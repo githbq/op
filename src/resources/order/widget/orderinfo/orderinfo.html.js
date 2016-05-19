e798282508aa67ba23455acd40580f78937af6b1
hubq <hubq@firstshare.cn>
2016-05-19 20:41:12 +0800

    BUG

--- src/resources/order/widget/orderinfo/orderinfo.html.js
+++ src/resources/order/widget/orderinfo/orderinfo.html.js
@@ -1,3 +1,3 @@
 define(function(require, exports, module) {
-    module.exports="<div class=\"widget-order-info\">\r\n <p>企业账号:<span class=\"ea\"></span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;企业名称:<span class=\"account\"></span> </p>\r\n\t<table class=\"order-tab\" style=\"border:none;\">\r\n <tbody></tbody>\r\n </table>\r\n\r\n</div>";
+    module.exports="<div class=\"widget-order-info\">\n <p>企业账号:<span class=\"ea\"></span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;企业名称:<span class=\"account\"></span> </p>\n\t<table class=\"order-tab\" style=\"border:none;\">\n <tbody></tbody>\n </table>\n\n</div>";
 });
