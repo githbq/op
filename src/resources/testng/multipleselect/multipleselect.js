a1b6dacce26597e26748de6ff8a7a198ba60ef02
hubq <hubq@firstshare.cn>
2016-06-01 12:00:18 +0800

    'select2'

--- src/resources/testng/multipleselect/multipleselect.js
+++ src/resources/testng/multipleselect/multipleselect.js
@@ -134,6 +134,7 @@ define(function (require, exports, module) {
         ];
 
         $scope.config3 = {
+            placeholder:'加载完毕',
             data: [{id: 1, text: 'bug'}, {id: 2, text: 'duplicate'}, {id: 3, text: 'invalid'}, {id: 4, text: 'wontfix'}]
             // 其他配置略，可以去看看内置配置中的ajax配置
         };
