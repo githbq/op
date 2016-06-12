2bd6ed2fea8240be926b37d8eb88a5d93a91aace
ligd <ligd@fxiaoke.com>
2016-06-12 10:27:18 +0800

    [还原订单列表]

--- src/resources/order/orderlist/orderlist.js
+++ src/resources/order/orderlist/orderlist.js
@@ -2,8 +2,12 @@ define( function( require, exports, module ) {
     var IBSS = window.IBSS,
         TplEvent = IBSS.tplEvent;
 
+    var page = require('common/widget/page/page');
+
     //实验ng模块
     var main = angular.module('main',[]);
+
+
 
     //---------------
     // 表格模块
@@ -35,8 +39,8 @@ define( function( require, exports, module ) {
                             "<tbody>"+
                                 "<tr ng-repeat='tr in data.tbody'>" +
 
-                                    "<td ng-repeat='td in tr'>{{td}}</td>" +
-                                	
+                                    "<td ng-repeat='td in tr.data'>{{td}}</td>" +
+                                	"<td>  <><>  </td>"
                                 	
                                 "</tr>" +
                             "</tbody>" +
@@ -50,12 +54,17 @@ define( function( require, exports, module ) {
 	main.controller('orderlist',['$scope', function($scope){
             
         console.log('controller test is working');
+
+        $scope.pagesize = 20;
+        $scope.pagenumber = 0;
+
+
         $scope.data = {};
         $scope.data.thead = ['订单号','合同号','企业名称','账号','订单类型','提单人','所属部门/代理商','特批单','当前审批节点','到款认领状态','订单状态','付费状态','提单日期','操作'];
         $scope.data.tbody = [
-                        [1,2,3,4,5],
-                        [2,3,4,5,6],
-                        [3,4,5,6,7]
+                        { 'data': [1,2,3,4,5] , 'action': [{'text':'收尾款'},{'text':'删除'},{'text':'查看'},{'text':'联合跟进人'},{'text':'退款'},{'text':'发票'}]},
+                        { 'data': [2,3,4,5,6] , 'action': [{'text':'收尾款'},{'text':'删除'},{'text':'查看'},{'text':'联合跟进人'},{'text':'退款'},{'text':'收尾款'}]},
+                        { 'data': [3,4,5,6,7] , 'action': [{'text':'收尾款'},{'text':'删除'},{'text':'查看'},{'text':'联合跟进人'},{'text':'退款'},{'text':'收尾款'}]}
                      ];
 
         //搜索订单列表
