ea230c5673761943ae7d09492b943c19b6ae6e61
ligd <ligd@fxiaoke.com>
2016-06-22 20:01:28 +0800

    [修复订单列表]

--- src/resources/order/openenterprise/openenterprise.js
+++ src/resources/order/openenterprise/openenterprise.js
@@ -2,10 +2,20 @@ define( function( require, exports, module ) {
     var IBSS = window.IBSS,
         TplEvent = IBSS.tplEvent;
 
+    var Page = require('../../testng/enterprisedetail/enterprisedetailmodule');
+
     exports.init = function() {
         var $el = exports.$el;
 
-        console.log('企业开通');
+        var p = new Page({wrapper: $el});
+        p.render();
+        //setTimeout(function () {
+        //    debugger
+        //    //p.nextStep();//下一步
+        //    //  p.prevStep();//上一步
+        //    //  p.hideFootBtns();//隐藏底部按钮
+        //    // p.hideTopBar();//隐藏头部第几步提示栏
+        //}, 5000)
     }
 } );
 
