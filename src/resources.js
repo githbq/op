e81e6a4abeac49d98372c85cef201d230e60e99c
hubq <hubq@firstshare.cn>
2016-06-28 10:39:45 +0800

    'resources'

--- src/resources.js
+++ src/resources.js
@@ -2,6 +2,9 @@ window.RESOURCES = {
     css: ['resources/assets/style/main.css'],
     js: [
         'resources/common/scripts/common.js',
-        'resources/assets/scripts/main.js'
+        'resources/assets/scripts/main.js',
+        'resources/common/scripts/lib/angular.js',
+        'resources/common/scripts/lib/angular-messages.js',
+        'resources/common/scripts/lib/angular-filter.js'
     ]
 };
