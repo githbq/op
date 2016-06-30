e7f1bac68b2880e1b8c3d19d5c2b2749f49fb729
李耕 <lig@firstshare.cn>
2016-06-30 14:47:47 +0800

    a

--- src/resources/common/widget/kindeditor/kindeditor-all.js
+++ src/resources/common/widget/kindeditor/kindeditor-all.js
@@ -7199,7 +7199,7 @@ KindEditor.plugin('image', function(K) {
 		allowImageRemote = K.undef(self.allowImageRemote, true),
 		formatUploadUrl = K.undef(self.formatUploadUrl, true),
 		allowFileManager = K.undef(self.allowFileManager, false),
-		uploadJson = K.undef(self.uploadJson, 'op/api/file/uploadsinglefile'),
+		uploadJson = K.undef(self.uploadJson, 'api/file/uploadsinglefile'),
 		imageTabIndex = K.undef(self.imageTabIndex, 0),
 		imgPath = self.pluginsPath + 'image/images/',
 		extraParams = K.undef(self.extraFileUploadParams, {}),
