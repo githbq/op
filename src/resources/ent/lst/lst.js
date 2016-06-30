655440ed7495019b0e9ae237d720114c7d2486f1
ligd <ligd@fxiaoke.com>
2016-06-30 21:52:02 +0800

    [repair]

--- src/resources/ent/lst/lst.js
+++ src/resources/ent/lst/lst.js
@@ -35,8 +35,8 @@ define( function( require, exports, module ) {
         //查看企业详情
         entList.on('detail',function( id , status ){
             
-            var entDetail = new EntDetail();
-            entDetail.show( id,status );
+            //var entDetail = new EntDetail();
+            //entDetail.show( id,status );
         });
 
         //查看跟踪记录
