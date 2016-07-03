e95a3ea8565f56909b1d060088ebc806747b81bc
ligd <ligd@fxiaoke.com>
2016-07-03 15:14:43 +0800

    [repair]

--- src/resources/order/detailpayment/detailpayment.js
+++ src/resources/order/detailpayment/detailpayment.js
@@ -202,6 +202,7 @@ define( function(require, exports, module){
 			 //收尾款模块
 			 //设置是否可以编辑
 			me.attrs.moneyEdit = me.attrs.options.editFlag;
+			
 			//财务驳回只能部分编辑和小助手第二次驳回
 			if(me.attrs.options.rejectsFrom &&  me.attrs.options.rejectsFrom == 3  && me.attrs.options.editFlag){
 				me.attrs.moneyEdit = false;;
